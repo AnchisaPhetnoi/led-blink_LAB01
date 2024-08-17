8888791c6016b3e3696e733ce083de1a97caed90
Anchisa Phetnoi <144197034+AnchisaPhetnoi@users.noreply.github.com>
2024-08-17 21:25:16 +0700

    first commit

---
+++ main/blink_example_main.c
@@ -0,0 +1,31 @@
+/* Blink Example
+
+   This example code is in the Public Domain (or CC0 licensed, at your option.)
+
+   Unless required by applicable law or agreed to in writing, this
+   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+   CONDITIONS OF ANY KIND, either express or implied.
+*/
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "driver/gpio.h"
+#include "esp_log.h"
+#include "led_strip.h"
+#include "sdkconfig.h"
+
+static const char *TAG = "example";
+
+
+void app_main(void)
+{
+
+    
+
+    while (1) {
+        printf("Hello ESP32\r\n")
+        vTaskDelay(CONFIG_BLINK_PERIOD / portTICK_PERIOD_MS);
+
+
+    }
+}
